6e073cfb3313137da14aa984f0c8a88bc97ecf30
alpha <glacruz5050@protonmail.com>
2021-10-23 11:01:48 -0500

    Changed debug

--- list/settings.py
+++ list/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l^@)cp)p$h!zg0(r4=n@5bv5vou5v9%3l&0a7m4g_h(&xfxxu$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["glc-tasks.herokuapp.com"]
 
@@ -151,6 +151,6 @@ LOGOUT_REDIRECT_URL = "main:welcome"
 django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
-    DEBUG = False
-elif os.environ.get('DEBUG') == 'FALSE':
     DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
