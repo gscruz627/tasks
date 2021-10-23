7b728cc375118eda8f8972ec2e8a063d2f2b9093
alpha <glacruz5050@protonmail.com>
2021-10-22 19:13:26 -0500

    error3535

--- list/settings.py
+++ list/settings.py
@@ -149,7 +149,6 @@ LOGOUT_REDIRECT_URL = "main:welcome"
 
 django_heroku.settings(locals())
 if os.environ.get('DEBUG') == 'TRUE':
-    DEBUG = True
-elif os.environ.get('DEBUG') == 'FALSE':
     DEBUG = False
-
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = True
